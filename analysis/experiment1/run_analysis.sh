3818c55cf44c0273ad9802b971afd1164da76eaa
T18369 <153779697+T18369@users.noreply.github.com>
2026-08-15 19:19:13 -0400

    Update analysis script to use new structure scripts

--- analysis/experiment1/run_analysis.sh
+++ analysis/experiment1/run_analysis.sh
@@ -4,8 +4,8 @@ cd gpmhc/analysis/experiment1
 
 echo "Running DQ structural analysis pipeline"
 
-python3 01_select_dq_structures.py
-python3 02_generate_af2_inputs.py
+python3 01_select_structures.py
+python3 02_generate_fasta.py
 python3 03_extract_distance_maps.py
 python3 04_contact_architecture.py
 
